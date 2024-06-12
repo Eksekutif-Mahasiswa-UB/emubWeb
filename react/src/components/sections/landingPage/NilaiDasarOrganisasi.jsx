fbead8308bc99911ed052a3b95c1465dba21f171
AiFahri <fahrinurilll@gmail.com>
2024-06-12 22:10:03 +0700

    fix background NilaiDasarOrganisasi

--- react/src/components/sections/landingPage/NilaiDasarOrganisasi.jsx
+++ react/src/components/sections/landingPage/NilaiDasarOrganisasi.jsx
@@ -4,14 +4,16 @@ import Rasional from "../../../assets/nilaiDasarOrganisasi/Rasional.png";
 import Progres from "../../../assets/nilaiDasarOrganisasi/Progres.png";
 import Dinamis from "../../../assets/nilaiDasarOrganisasi/Dinamis.png";
 import Terbuka from "../../../assets/nilaiDasarOrganisasi/Terbuka.png";
+import begron from "../../../assets/nilaiDasarOrganisasi/Value.png";
 const NilaiDasarOrganisasi = () => {
-
-    
-
     return (
         <section className="lg:mt-10 mt-5 px-4 lg:px-24 container mx-auto ">
-        
-            <div className="w-full  bg-gradient-to-br from-primary-skyBlue mx-auto shadow-2xl p-4 lg:p-8 rounded-xl to-white">
+            <div
+                className="w-full mx-auto  p-4 lg:p-8 rounded-xl "
+                style={{
+                    backgroundImage: `url(${begron})`,
+                }}
+            >
                 <h1 className="font-helvetica-extraBold text-xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r  inline-block from-primary-charcoalGray to-primary-tealBlue mb-5 lg:mb-10 ">
                     Nilai Dasar Organisasi
                 </h1>
@@ -27,14 +29,12 @@ const NilaiDasarOrganisasi = () => {
                             alt="Obyektif"
                             draggable="false"
                         />
-                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 ">
-                          
-                        </div>
+                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 "></div>
                     </div>
-                  
+
                     <div className="w-[45%] lg:w-1/4 rounded-xl shadow-2xl p-2 lg:p-4 relative group aspect-[16/15] overflow-hidden bg-[#2D9596]">
                         <h3 className="text-base lg:text-4xl text-shadow text-white font-helvetica-regular font-bold group-hover:text-primary-tealBlue duration-500 z-20 relative ease-in-out  ">
-                          Rasional
+                            Rasional
                         </h3>
                         <img
                             src={Rasional}
@@ -43,14 +43,12 @@ const NilaiDasarOrganisasi = () => {
                             alt="Rasional"
                             draggable="false"
                         />
-                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 ">
-                          
-                        </div>
+                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 "></div>
                     </div>
-                  
+
                     <div className="w-[45%] lg:w-1/4 rounded-xl shadow-2xl p-2 lg:p-4 relative group aspect-[16/15] overflow-hidden bg-[#628E90]">
                         <h3 className="text-base lg:text-4xl text-shadow text-white font-helvetica-regular font-bold group-hover:text-primary-tealBlue duration-500 z-20 relative ease-in-out  ">
-                          Progres
+                            Progres
                         </h3>
                         <img
                             src={Progres}
@@ -59,11 +57,9 @@ const NilaiDasarOrganisasi = () => {
                             alt="Progres"
                             draggable="false"
                         />
-                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 ">
-                           
-                        </div>
+                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 "></div>
                     </div>
-                  
+
                     <div className="w-[45%] lg:w-1/4 rounded-xl shadow-2xl p-2 lg:p-4 relative group aspect-[16/15] overflow-hidden bg-[#007B8E]">
                         <h3 className="text-base lg:text-4xl text-shadow text-white font-helvetica-regular font-bold group-hover:text-primary-tealBlue duration-500 z-20 relative ease-in-out  ">
                             Dinamis
@@ -75,11 +71,9 @@ const NilaiDasarOrganisasi = () => {
                             alt="Dinamis"
                             draggable="false"
                         />
-                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 ">
-                        
-                        </div>
+                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 "></div>
                     </div>
-                  
+
                     <div className="w-[45%] lg:w-1/4 rounded-xl shadow-2xl p-2 lg:p-4 relative group aspect-[16/15] overflow-hidden bg-[#176B87]">
                         <h3 className="text-base lg:text-4xl text-shadow text-white font-helvetica-regular font-bold group-hover:text-primary-tealBlue duration-500 z-20 relative ease-in-out  ">
                             Terbuka
@@ -91,12 +85,8 @@ const NilaiDasarOrganisasi = () => {
                             alt="Terbuka"
                             draggable="false"
                         />
-                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 ">
-                          
-                        </div>
+                        <div className="w-full h-0 flex flex-col justify-end absolute bottom-0 left-0 origin-bottom duration-500 ease-in-out group-hover:h-full bg-[#DCF7FB] z-10 group-hover:py-4 group px-4 "></div>
                     </div>
-                  
-                  
                 </div>
             </div>
         </section>
--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -4,8 +4,10 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  
   theme: {
     extend: {
+      
       colors: {
         primary :{
           charcoalGray: "#202020",
